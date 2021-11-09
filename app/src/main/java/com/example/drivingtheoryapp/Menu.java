1ae940c429d58bf2a1d59f86be875093e0d6de52
Thomas Cudden <tcudden01@qub.ac.uk>
2021-11-09 22:52:52 +0000

    Menu Screen buttons

--- app/src/main/java/com/example/drivingtheoryapp/Menu.java
+++ app/src/main/java/com/example/drivingtheoryapp/Menu.java
@@ -2,7 +2,11 @@ package com.example.drivingtheoryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Menu extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Button logoutbtn = (Button) findViewById(R.id.logoutbtn);
+
+
+        logoutbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logOut();
+            }
+        });
+
     }
+
+
+    public void logOut(){
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+    }
+
+
 }
