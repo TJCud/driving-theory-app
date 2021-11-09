c1df0920bb01f86619c216ccf018a4d501eabea4
Thomas Cudden <tcudden01@qub.ac.uk>
2021-11-09 01:51:34 +0000

    Simple login page

--- app/src/main/java/com/example/drivingtheoryapp/MainActivity.java
+++ app/src/main/java/com/example/drivingtheoryapp/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.drivingtheoryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
+import android.widget.Button;
+
+
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
+
+        Button loginbtn = (Button) findViewById(R.id.loginbtn);
+
+        //admin and admin
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                    //correct
+                    Toast.makeText(MainActivity.this, "Login Successful",Toast.LENGTH_SHORT).show();
+
+
+                }else
+                    //incorrect
+                    Toast.makeText(MainActivity.this, "Login Failed",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
     }
 }
